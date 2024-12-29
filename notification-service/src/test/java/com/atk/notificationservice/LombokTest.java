3e99946ad24867181022cb5582cc4e639bd7d166
Ahmet <AhmetTK4@users.noreply.github.com>
2024-12-29 23:07:14 +0300

    new build
    new build

---
+++ notification-service/src/test/java/com/atk/notificationservice/LombokTest.java
@@ -0,0 +1,10 @@
+package com.atk.notificationservice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LombokTest {
+    public static void main(String[] args) {
+        log.info("Lombok is working!");
+    }
+}
